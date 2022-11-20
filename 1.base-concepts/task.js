1412cb2a6ad61600f33c6347dd10d56b93f95912
TimofeevT <timoha1075@gmail.com>
2022-11-20 19:35:39 +0300

    added task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,21 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  var D= Math.pow(b , 2) -4*a*c;
+
+  if (D>0) {
+    arr = [(-b + Math.sqrt(D)) / (2*a) , (-b - Math.sqrt(D)) / (2*a)];
+    return arr; 
+  } 
+   else if (D===0) {
+   arr = [-b / (2*a)];
+   return arr; 
+  }
+    else if (D<0) {
+    arr = [];
+    return arr; // array
+   }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
